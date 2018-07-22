9049cc045acd6609722ceb25d30baae44093dd70
Christopher George <cmgeorge@andrew.cmu.edu>
2018-07-22 17:45:52 -0400

    Create Sphere.py

---
+++ Sphere.py
@@ -0,0 +1,11 @@
+from PyMaya import *
+import maya.cmds as cmds
+from pymel.core import *
+
+class Sphere(Shape):
+    def __init__(self, centerX, centerY, centerZ, width=1, height=1, depth=1,
+                            angleX=0, angleY=0, angleZ=0, fill=rgb(255, 255, 255)):
+        shape = polySphere()
+        super(Sphere, self).__init__(shape, centerX, centerY, centerZ, width=width, 
+                                    height=height, depth=depth, angleX=angleX, 
+                                    angleY=angleY, angleZ=angleZ, fill=fill)
